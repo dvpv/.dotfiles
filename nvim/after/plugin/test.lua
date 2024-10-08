78562aa7a417926ccf41278fe6d91f6266f30f2d
dvpv <david.pava@protonmail.com>
2024-10-08 16:43:36 +0300

    adding keymap for run nearest test with debugger

--- nvim/after/plugin/test.lua
+++ nvim/after/plugin/test.lua
@@ -31,6 +31,7 @@ neotest.setup({
 vim.keymap.set("n", "<leader>tt", function() neotest.run.run(vim.fn.expand("%")) end)
 vim.keymap.set("n", "<leader>tT", function() neotest.run.run(vim.uv.cwd()) end)
 vim.keymap.set("n", "<leader>tr", function() neotest.run.run() end)
+vim.keymap.set("n", "<leader>td", function() neotest.run.run({ strategy = "dap" }) end)
 vim.keymap.set("n", "<leader>tl", function() neotest.run.run_last() end)
 vim.keymap.set("n", "<leader>ts", function() neotest.summary.toggle() end)
 vim.keymap.set("n", "<leader>to", function() neotest.output.open({ enter = true, auto_close = true }) end)
