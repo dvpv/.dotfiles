f4a56423d00dc3533d0e34a68ffe665bd508204b
dvpv <david.pava@protonmail.com>
2024-10-28 20:44:26 +0200

    Adding package managers installation and one line install to readme

--- tmux/config_install.sh
+++ tmux/config_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Linking tmux"
+echo "[TMUX] Downloading TPM"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "[TMUX] Linking"
 rm -rf $HOME/.config/tmux
+mkdir -p $HOME/.config
 ln -s $(pwd) $HOME/.config/tmux
 
